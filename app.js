14d1a51e799c949d54770ad7b5948c6f81f47fe9
HELA TALBI <hagartalbi@gmail.com>
2022-08-21 13:59:00 +0100

    local storage

--- app.js
+++ app.js
@@ -25,3 +25,29 @@ document.querySelector(".delete-item secondary-content").addEventListener("click
 function removeTask(e) {
    console.log(e.target)
 }
+
+
+
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault()
+    const task = document.getElementById("task").value
+  
+    let tasks
+  
+    if (localStorage.getItem("tasks") === null) {
+      tasks = []
+    } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+  
+    tasks.push(task)
+  
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    alert("Task Saved")
+  })
+  
+  const tasks = JSON.parse(localStorage.getItem("tasks"))
+  
+  tasks.forEach((task) => {
+    console.log(task)
+  })
